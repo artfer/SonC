1a2e4563a18a856180f63bec99434d60a22983ec
artfer <artureco95.af@gmail.com>
2018-04-02 20:17:44 +0100

    peido

--- ShipsOnChips/src/sonc/battle/ShipCommand.java
+++ ShipsOnChips/src/sonc/battle/ShipCommand.java
@@ -2,7 +2,7 @@ package sonc.battle;
 
 
 /**
- * This is the abstract part of the <b>Command<b> design pattern.
+ * This is the abstract part of the <b>Command</b> design pattern.
  * It defines a command executed by the ship and is used to delay
  * command execution until the {@link Ship#move()} command is 
  * completed. Ship commands are used to ensure that ships execute
