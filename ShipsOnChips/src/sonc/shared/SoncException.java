33318b5b9ab5f7ce49ebaae586bf8bfc0fb17d53
artfer <artureco95.af@gmail.com>
2018-04-02 17:38:51 +0100

    SoncException class created with all methods but all empty.

---
+++ ShipsOnChips/src/sonc/shared/SoncException.java
@@ -0,0 +1,22 @@
+package sonc.shared;
+
+import java.lang.*;
+
+public class SoncException extends Exception{
+	public SoncException(String message,Throwable cause,
+			boolean enableSuppression,boolean writeableStackTrace){
+		
+	}
+	
+	public SoncException(String massage,Throwable cause) {
+		
+	}
+	
+	public SoncException(String message) {
+		
+	}
+	
+	public SoncException(Throwable cause) {
+		
+	}
+}
